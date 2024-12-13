86b050f467cb4542e8955b5b778aad840b469961
wuyuanye <wuyuanye@boosterobotics.com>
2024-12-13 15:56:47 +0800

    feat: support hand control api

--- include/booster_interface/booster_interface/message_utils.hpp
+++ include/booster_interface/booster_interface/message_utils.hpp
@@ -2,58 +2,92 @@
 
 #include "booster_interface/msg/booster_api_req_msg.hpp"
 #include <booster/robot/common/robot_shared.hpp>
+#include <booster/robot/common/entities.hpp>
 #include <booster/robot/b1/b1_api_const.hpp>
 #include <booster/robot/b1/b1_loco_api.hpp>
 
 namespace booster_interface {
 
-msg::BoosterApiReqMsg ConstructMsg(booster::robot::b1::LocoApiId api_id, nlohmann::json json_body) {
+using namespace booster::robot::b1;
+using namespace booster::robot;
+
+msg::BoosterApiReqMsg ConstructMsg(LocoApiId api_id, std::string json_body) {
     msg::BoosterApiReqMsg msg;
     msg.api_id = static_cast<int64_t>(api_id);
-    msg.body = json_body.dump();
+    msg.body = json_body;
     return msg;
 }
 
 msg::BoosterApiReqMsg CreateChangeModeMsg(booster::robot::RobotMode mode) {
-    nlohmann::json body;
-    body["mode"] = static_cast<int>(mode);
-    return ConstructMsg(booster::robot::b1::LocoApiId::kChangeMode, body);
+    ChangeModeParameter change_mode(mode);
+    std::string param = change_mode.ToJson().dump();
+    return ConstructMsg(LocoApiId::kChangeMode, param);
 }
 
 msg::BoosterApiReqMsg CreateMoveMsg(float vx, float vy, float vyaw) {
-    nlohmann::json body;
-    body["vx"] = vx;
-    body["vy"] = vy;
-    body["vyaw"] = vyaw;
-    return ConstructMsg(booster::robot::b1::LocoApiId::kMove, body);
+    MoveParameter move(vx, vy, vyaw);
+    std::string param = move.ToJson().dump();
+    return ConstructMsg(LocoApiId::kMove, param);
 }
 
 msg::BoosterApiReqMsg CreateRotateHeadMsg(float pitch, float yaw) {
-    nlohmann::json body;
-    body["pitch"] = pitch;
-    body["yaw"] = yaw;
-    return ConstructMsg(booster::robot::b1::LocoApiId::kRotateHead, body);
+    RotateHeadParameter head_ctrl(pitch, yaw);
+    std::string param = head_ctrl.ToJson().dump();
+    return ConstructMsg(LocoApiId::kRotateHead, param);
 }
 
 msg::BoosterApiReqMsg CreateRotateHeadWithDirectionMsg(int pitch_direction, int yaw_direction) {
-    nlohmann::json body;
-    body["pitch_direction"] = pitch_direction;
-    body["yaw_direction"] = yaw_direction;
-    return ConstructMsg(booster::robot::b1::LocoApiId::kRotateHeadWithDirection, body);
+    RotateHeadWithDirectionParameter head_ctrl(pitch_direction, yaw_direction);
+    std::string param = head_ctrl.ToJson().dump();
+    return ConstructMsg(LocoApiId::kRotateHeadWithDirection, param);
 }
 
-msg::BoosterApiReqMsg CreateWaveHandMsg(booster::robot::b1::HandIndex hand_index, booster::robot::b1::HandAction hand_action) {
-    nlohmann::json body;
-    body["hand_index"] = static_cast<int>(hand_index);
-    body["hand_action"] = static_cast<int>(hand_action);
-    return ConstructMsg(booster::robot::b1::LocoApiId::kWaveHand, body);
+msg::BoosterApiReqMsg CreateWaveHandMsg(HandIndex hand_index, HandAction hand_action) {
+    WaveHandParameter wave_hand(hand_index, hand_action);
+    std::string body = wave_hand.ToJson().dump();
+    return ConstructMsg(LocoApiId::kWaveHand, body);
 }
 
 msg::BoosterApiReqMsg CreateLieDownMsg() {
-    return ConstructMsg(booster::robot::b1::LocoApiId::kLieDown, "");
+    return ConstructMsg(LocoApiId::kLieDown, "");
 }
 
 msg::BoosterApiReqMsg CreateGetUpMsg() {
-    return ConstructMsg(booster::robot::b1::LocoApiId::kGetUp, "");
+    return ConstructMsg(LocoApiId::kGetUp, "");
 }
+
+msg::BoosterApiReqMsg CreateMoveHandEndEffectorWithAuxMsg(
+    const Posture &target_posture,
+    const Posture &aux_posture,
+    int time_millis,
+    HandIndex hand_index) {
+    MoveHandEndEffectorParameter move_hand(target_posture, aux_posture, time_millis, hand_index);
+    std::string param = move_hand.ToJson().dump();
+    return ConstructMsg(LocoApiId::kMoveHandEndEffector, param);
+}
+
+msg::BoosterApiReqMsg CreateMoveHandEndEffectorMsg(
+    const Posture &target_posture,
+    int time_millis,
+    HandIndex hand_index) {
+    MoveHandEndEffectorParameter move_hand(target_posture, time_millis, hand_index);
+    std::string param = move_hand.ToJson().dump();
+    return ConstructMsg(LocoApiId::kMoveHandEndEffector, param);
+}
+
+msg::BoosterApiReqMsg CreateControlGripperMsg(
+    const GripperMotionParameter &motion_param,
+    GripperControlMode mode,
+    HandIndex hand_index) {
+    ControlGripperParameter control_gripper(motion_param, mode, hand_index);
+    std::string param = control_gripper.ToJson().dump();
+    return ConstructMsg(LocoApiId::kControlGripper, param);
+}
+
+msg::BoosterApiReqMsg CreateSwitchHandEndEffectorControlModeMsg(bool switch_on) {
+    SwitchHandEndEffectorControlModeParameter switch_param(switch_on);
+    std::string param = switch_param.ToJson().dump();
+    return ConstructMsg(LocoApiId::kSwitchHandEndEffectorControlMode, param);
+}
+
 } // namespace booster_interface
